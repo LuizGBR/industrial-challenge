399ebd27525d458620265fbf6818d7ef02057d6f
LuizGBR <bandeiralg13@gmail.com>
2022-06-17 01:35:30 -0300

    Main page base

--- desafio-industriall/src/pages/MainPage/index.tsx
+++ desafio-industriall/src/pages/MainPage/index.tsx
@@ -1,5 +1,6 @@
 import './style.scss'
 import logoImg from '../../assets/images/logo-escura.svg'
+import AddIcon from '@mui/icons-material/Add';
 
 export function MainPage(){
     return(
@@ -11,9 +12,17 @@ export function MainPage(){
             </header>
            <main>
                 <div className="main-header">
-                    <h1>Atas de Reunião</h1>
-                    <p>Estas são as atas das últimas reuniões.</p>
-                    <button>NOVA ATA</button>
+                    <div>
+                        <div>
+                            <h1>Atas de Reunião</h1>
+                            <p>Estas são as atas das últimas reuniões.</p>
+                        </div> 
+                        <button type="button">
+                            <AddIcon fontSize="small" />
+                            NOVA ATA
+                        </button>  
+                    </div>
+                    
                 </div>
            </main>
         </div>
